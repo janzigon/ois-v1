c4a893d045372a51706f7bec4bd57ecd8588f413
KrNiko <kraljniko24@gmail.com>
2024-03-03 17:39:31 +0100

    Dodaj izpis pospeška.

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,5 +3,9 @@ public class Gravitacija{
 
 		System.out.printf("OIS je zakon!");
 
+public static void izpis(double visina){
+System.out.printf("Gravitacijski pospešek na %.2f km nadmorske višine je %.2f m/s^2\n", visina, izracunGravitacije(visina * 1000));
+}
+
 	}
 }
